9b2f108f1d62c69fe8af4bc7eabe5ff1b872ff11
bahrynowskijakub <bahrynowski.praca@gmail.com>
2021-11-14 00:48:18 +0100

    asdas

--- ClientNew/src/app/app.module.ts
+++ ClientNew/src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-=======
-import { HttpClientModule } from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
->>>>>>> 3a613ca738840f39440cbf5b487a7c50bcc60a2b
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './core/sidebar/sidebar.component';
@@ -15,16 +10,7 @@ import { MainComponent } from './core/main/main.component';
 import { LoginComponent } from './core/login/login.component';
 
 @NgModule({
-<<<<<<< HEAD
   declarations: [AppComponent, SidebarComponent, MainComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-=======
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    MainComponent,
-    LoginComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -32,7 +18,6 @@ import { LoginComponent } from './core/login/login.component';
     FormsModule,
     ReactiveFormsModule
   ],
->>>>>>> 3a613ca738840f39440cbf5b487a7c50bcc60a2b
   providers: [],
   bootstrap: [AppComponent]
 })
--- ClientNew/src/app/core/login/login.component.ts
+++ ClientNew/src/app/core/login/login.component.ts
@@ -1,17 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { WorkersService } from '../../workers/workers.service';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-
-interface User {
-  email: string;
-  password: string;
-}
-=======
-import {HttpClient} from '@angular/common/http';
 import { FormBuilder, FormGroup } from '@angular/forms';
->>>>>>> 3a613ca738840f39440cbf5b487a7c50bcc60a2b
 
 @Component({
   selector: 'app-login',
@@ -22,31 +11,20 @@ export class LoginComponent implements OnInit {
   url = 'https://localhost:5001/api/login';
   form: FormGroup;
   model: any = {};
-  constructor(
-    private http: HttpClient,
-    private formBuilder: FormBuilder,
-    ) {}
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       email: '',
-      password: '',
+      password: ''
     });
   }
 
   login() {
-    console.log("12333");
+    console.log('12333');
 
-<<<<<<< HEAD
-  checkIfUserExists(user) {
-    this.workersService.checkIfUserExists(user).subscribe(res => {
-      console.log(res);
+    this.http.post(this.url, this.form.getRawValue()).subscribe(() => {
+      console.log('123');
     });
-=======
-    this.http.post(this.url,this.form.getRawValue()).subscribe(()=>{
-      console.log("123");
-    }
-    );
->>>>>>> 3a613ca738840f39440cbf5b487a7c50bcc60a2b
   }
 }
