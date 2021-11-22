421d851b77a41ba60ebad893c2b6f493c23d95b4
KarolZirex <karol.dura1@wp.pl>
2021-11-22 20:35:29 +0100

    add DeleteEmployee Endpoint

---
+++ src/HRMOptimus.Application/Employee/Command/RemoveEmployee/RemoveEmployeeCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using HRMOptimus.Application.Common.Exceptions;
+using HRMOptimus.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRMOptimus.Application.Employee.Command.RemoveEmployee
+{
+    public class RemoveEmployeeCommandHandler : IRequestHandler<RemoveEmployeeCommand>
+    {
+        private readonly IHRMOptimusDbContext _context;
+
+        public RemoveEmployeeCommandHandler(IHRMOptimusDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(RemoveEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            var employee = await _context.Employees.FirstOrDefaultAsync(x => x.Id == request.EmployeeId);
+
+            if (employee == null)
+                throw new NotFoundException("There is no employee with Id: " + request.EmployeeId);
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
