16be8565297e800bacf363024aa8eaf5971fd93c
bahrynowskijakub <bahrynowski.praca@gmail.com>
2021-11-23 15:09:29 +0100

    edit user view

--- ClientNew/src/app/employees/employees.service.ts
+++ ClientNew/src/app/employees/employees.service.ts
@@ -10,7 +10,10 @@ interface Worker {
   providedIn: 'root'
 })
 export class EmployeesService {
-  getEmployeesurl = 'https://localhost:5001/api/employees';
+  getEmployeesurl =
+    'https://localhost:5001/api/employees?PageNumber=1&PageSize=30&SortBy=FullName&SortDirection=1';
+  getEmployeeUrl = 'https://localhost:5001/api/employee/details';
+  deleteEmployeeUrl = 'https://localhost:5001/api/employee/delete';
   constructor(private http: HttpClient) {}
 
   getEmployees(): Observable<any> {
@@ -18,6 +21,29 @@ export class EmployeesService {
       .get(this.getEmployeesurl, {
         headers: {
           'Content-Type': 'applicastion/json'
+        }
+      })
+      .pipe(map((res: any) => res));
+  }
+
+  getEmployee(id): Observable<any> {
+    return this.http
+      .get(this.getEmployeeUrl, {
+        headers: {
+          'Content-Type': 'applicastion/json'
+        },
+        params: {
+          employeeId: id
+        }
+      })
+      .pipe(map((res: any) => res));
+  }
+
+  deleteEmployee(id): Observable<any> {
+    return this.http
+      .delete(this.deleteEmployeeUrl, {
+        params: {
+          employeeId: id
         }
       })
       .pipe(map((res: any) => res));
