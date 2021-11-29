0d65ddc42edafe89f9bcc5c2ebd3a38c65c4a43c
hegemon76 <adrian.dawid76@gmail.com>
2021-11-29 19:23:26 +0100

    fix delete leave register returns leave days

--- src/HRMOptimus.Application/LeaveRegister/Command/DeleteLeaveRegister/DeleteLeaveRegisterCommandHandler.cs
+++ src/HRMOptimus.Application/LeaveRegister/Command/DeleteLeaveRegister/DeleteLeaveRegisterCommandHandler.cs
@@ -3,27 +3,39 @@ using MediatR;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using HRMOptimus.Domain.Enums;
+using System;
 
 namespace HRMOptimus.Application.LeaveRegister.Command.DeleteLeaveRegister
 {
     class DeleteLeaveRegisterCommandHandler : IRequestHandler<DeleteLeaveRegisterCommand, int>
     {
         private readonly IHRMOptimusDbContext _context;
+        private readonly IUserContextService _userContextService;
 
-        public DeleteLeaveRegisterCommandHandler(IHRMOptimusDbContext context)
+        public DeleteLeaveRegisterCommandHandler(IHRMOptimusDbContext context, IUserContextService userContextService)
         {
             _context = context;
+            _userContextService = userContextService;
         }
         public async Task<int> Handle(DeleteLeaveRegisterCommand request, CancellationToken cancellationToken)
         {
             try
             {
                 var leaveRegister = _context.LeavesRegister.FirstOrDefault(x => x.Id == request.DeleteLeaveRegisterVm.Id);
+                var employee = _context.Employees.FirstOrDefault(x => x.Id == _userContextService.GetEmployeeId);
+
+                if (leaveRegister.IsApproved == IsApproved.Approved)
+                {
+                    employee.LeaveDaysLeft += leaveRegister.Duration;
+                }
+
+                _context.Employees.Update(employee);
                 _context.LeavesRegister.Remove(leaveRegister);
                 await _context.SaveChangesAsync(cancellationToken);
                 return 1;
             }
-            catch (System.Exception)
+            catch (Exception)
             {
                 return 0;
             }
