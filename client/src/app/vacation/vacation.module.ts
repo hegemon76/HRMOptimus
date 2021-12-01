95ba9435ede441cffdec52cb3fd7e34bd235554e
bahrynowskijakub <bahrynowski.praca@gmail.com>
2021-12-01 02:02:36 +0100

    vacations continue

--- client/src/app/vacation/vacation.module.ts
+++ client/src/app/vacation/vacation.module.ts
@@ -8,11 +8,8 @@ import { SharedModule } from '../shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-<<<<<<< HEAD
 import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-=======
->>>>>>> 5270fcd57c57f6d6b7f5c20cce7c2ddcb7cd9170
 
 @NgModule({
   declarations: [VacationComponent],
@@ -26,13 +23,9 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     ReactiveFormsModule,
     MatDatepickerModule,
     MatDatepickerModule,
-<<<<<<< HEAD
     MatNativeDateModule,
     MatSelectModule,
     MatProgressSpinnerModule
-=======
-    MatNativeDateModule
->>>>>>> 5270fcd57c57f6d6b7f5c20cce7c2ddcb7cd9170
   ],
   providers: []
 })
