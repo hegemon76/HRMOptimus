8459503897e0a454b0d31205c6954f0102e8ff29
hegemon76 <adrian.dawid76@gmail.com>
2021-12-08 23:05:17 +0100

    add roles

--- src/HRMOptimus.Persistance/HRMOptimusDbContext.cs
+++ src/HRMOptimus.Persistance/HRMOptimusDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using HRMOptimus.Application.Common.Interfaces;
 using HRMOptimus.Domain.Common;
 using HRMOptimus.Domain.Entities;
+using HRMOptimus.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -57,6 +59,10 @@ namespace HRMOptimus.Persistance
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(HRMOptimusDbContext).Assembly);
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole { Name = Domain.Enums.UserRoles.Administrator.ToString(), NormalizedName = Domain.Enums.UserRoles.Administrator.ToString() },
+                new IdentityRole { Name = Domain.Enums.UserRoles.User.ToString(), NormalizedName = Domain.Enums.UserRoles.User.ToString() }
+                );
         }
     }
 }
