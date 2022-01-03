32790ff1611f1e21c3bcb7268f81dbdaf033e7f7
hegemon76 <adrian.dawid76@gmail.com>
2022-01-04 00:41:52 +0100

    Add contract edit endpoint

---
+++ src/HRMOptimus.Application/Contract/Command/EditContract/EditContractValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMOptimus.Application.Contract.Command.EditContract
+{
+    public class EditContractValidator : AbstractValidator<EditContractVm>
+    {
+        public EditContractValidator()
+        {
+            RuleFor(x => x.ContractId).NotEmpty();
+        }
+    }
+}
