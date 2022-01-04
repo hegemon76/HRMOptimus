9c87d1796a6d508fe2338aa950a9d2b160c7355a
KarolZirex <karol.dura1@wp.pl>
2022-01-04 23:02:13 +0100

    refactor

--- src/HRMOptimus.Application/Employee/Command/EditEmployee/EditEmployeeCommandHandler.cs
+++ src/HRMOptimus.Application/Employee/Command/EditEmployee/EditEmployeeCommandHandler.cs
@@ -11,7 +11,6 @@ namespace HRMOptimus.Application.Employee.Command.EditEmployee
 {
     public class EditEmployeeCommandHandler : IRequestHandler<EditEmployeeCommand, Unit>
     {
-        private readonly UserManager<ApplicationUser> _userManager;
         private readonly IHRMOptimusDbContext _context;
 
         public EditEmployeeCommandHandler(UserManager<ApplicationUser> userManager, IHRMOptimusDbContext context)
@@ -22,59 +21,55 @@ namespace HRMOptimus.Application.Employee.Command.EditEmployee
 
         public async Task<Unit> Handle(EditEmployeeCommand request, CancellationToken cancellationToken)
         {
-            var user = _context.Employees.Include(a => a.Address)
+            var user = _context.Employees
+                .Include(a => a.Address)
                 .Include(app => app.ApplicationUser)
                 .FirstOrDefault(x => x.Id == request.Employee.EmployeeId);
-            if (user != null)
-            {
-                var updatedApplicationUser = user.ApplicationUser;
-                updatedApplicationUser.PhoneNumber =
-                    !string.IsNullOrWhiteSpace(request.Employee.PhoneNumber) ? request.Employee.PhoneNumber : updatedApplicationUser.PhoneNumber;
 
-                var updatedEmployee = user;
-                updatedEmployee.FirstName =
-                    !string.IsNullOrWhiteSpace(request.Employee.FirstName) ? request.Employee.FirstName : updatedEmployee.FirstName;
+            var updatedApplicationUser = user.ApplicationUser;
+            updatedApplicationUser.PhoneNumber =
+                !string.IsNullOrWhiteSpace(request.Employee.PhoneNumber) ? request.Employee.PhoneNumber : updatedApplicationUser.PhoneNumber;
 
-                updatedEmployee.LastName =
-                   !string.IsNullOrWhiteSpace(request.Employee.LastName) ? request.Employee.LastName : updatedEmployee.LastName;
+            var updatedEmployee = user;
+            updatedEmployee.FirstName =
+                !string.IsNullOrWhiteSpace(request.Employee.FirstName) ? request.Employee.FirstName : updatedEmployee.FirstName;
 
-                updatedEmployee.BirthDate =
-                    request.Employee.BirthDate.HasValue ? request.Employee.BirthDate.Value : updatedEmployee.BirthDate;
+            updatedEmployee.LastName =
+               !string.IsNullOrWhiteSpace(request.Employee.LastName) ? request.Employee.LastName : updatedEmployee.LastName;
 
-                updatedEmployee.Gender =
-                    request.Employee.Gender.HasValue ? request.Employee.Gender.Value : updatedEmployee.Gender;
+            updatedEmployee.BirthDate =
+                request.Employee.BirthDate.HasValue ? request.Employee.BirthDate.Value : updatedEmployee.BirthDate;
 
+            updatedEmployee.Gender =
+                request.Employee.Gender.HasValue ? request.Employee.Gender.Value : updatedEmployee.Gender;
 
-                var updatedAddress = updatedEmployee.Address;
+            var updatedAddress = updatedEmployee.Address;
 
-                updatedAddress.ZipCode =
-                   !string.IsNullOrWhiteSpace(request.Employee.ZipCode) ? request.Employee.ZipCode : updatedAddress.ZipCode;
+            updatedAddress.ZipCode =
+               !string.IsNullOrWhiteSpace(request.Employee.ZipCode) ? request.Employee.ZipCode : updatedAddress.ZipCode;
 
-                updatedAddress.City =
-                  !string.IsNullOrWhiteSpace(request.Employee.City) ? request.Employee.City : updatedAddress.City;
+            updatedAddress.City =
+              !string.IsNullOrWhiteSpace(request.Employee.City) ? request.Employee.City : updatedAddress.City;
 
-                updatedAddress.Street =
-                  !string.IsNullOrWhiteSpace(request.Employee.Street) ? request.Employee.Street : updatedAddress.Street;
+            updatedAddress.Street =
+              !string.IsNullOrWhiteSpace(request.Employee.Street) ? request.Employee.Street : updatedAddress.Street;
 
-                updatedAddress.BuildingNumber =
-                  !string.IsNullOrWhiteSpace(request.Employee.BuildingNumber) ? request.Employee.BuildingNumber : updatedAddress.BuildingNumber;
+            updatedAddress.BuildingNumber =
+              !string.IsNullOrWhiteSpace(request.Employee.BuildingNumber) ? request.Employee.BuildingNumber : updatedAddress.BuildingNumber;
 
-                updatedAddress.HouseNumber =
-                  !string.IsNullOrWhiteSpace(request.Employee.HouseNumber) ? request.Employee.HouseNumber : updatedAddress.HouseNumber;
+            updatedAddress.HouseNumber =
+              !string.IsNullOrWhiteSpace(request.Employee.HouseNumber) ? request.Employee.HouseNumber : updatedAddress.HouseNumber;
 
-                updatedAddress.Country =
-                  !string.IsNullOrWhiteSpace(request.Employee.Country) ? request.Employee.Country : updatedAddress.Country;
+            updatedAddress.Country =
+              !string.IsNullOrWhiteSpace(request.Employee.Country) ? request.Employee.Country : updatedAddress.Country;
 
+            var employee = _context.Employees.FirstOrDefault(e => e.Id == updatedEmployee.Id);
 
-                var employee = _context.Employees.FirstOrDefault(e => e.Id == updatedEmployee.Id);
+            _context.Employees.Update(updatedEmployee);
+            _context.Addresses.Update(updatedEmployee.Address);
+            _context.ApplicationUsers.Update(updatedApplicationUser);
 
-                _context.Employees.Update(updatedEmployee);
-                _context.Addresses.Update(updatedEmployee.Address);
-                _context.ApplicationUsers.Update(updatedApplicationUser);
-
-                await _context.SaveChangesAsync(cancellationToken);
-                return Unit.Value;
-            }
+            await _context.SaveChangesAsync(cancellationToken);
             return Unit.Value;
         }
     }
