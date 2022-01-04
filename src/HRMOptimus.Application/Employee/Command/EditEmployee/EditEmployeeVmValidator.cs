04c0121d861cec2104de70d2361c85232365f634
hegemon76 <adrian.dawid76@gmail.com>
2022-01-04 22:32:33 +0100

    refactor updating and validations

--- src/HRMOptimus.Application/Employee/Command/EditEmployee/EditEmployeeVmValidator.cs
+++ src/HRMOptimus.Application/Employee/Command/EditEmployee/EditEmployeeVmValidator.cs
@@ -1,25 +1,17 @@
 ﻿using FluentValidation;
-using HRMOptimus.Application.Common.Interfaces;
+using System;
 
 namespace HRMOptimus.Application.Employee.Command.EditEmployee
 {
     public class EditEmployeeVmValidator : AbstractValidator<EditEmployeeVm>
     {
-        public EditEmployeeVmValidator(IHRMOptimusDbContext context)
+        public EditEmployeeVmValidator()
         {
-            RuleFor(x => x.BirthDate).NotEmpty();
-            RuleFor(x => x.BuildingNumber).NotEmpty();
-            RuleFor(x => x.City).NotEmpty();
-            RuleFor(x => x.Country).NotEmpty();
-            RuleFor(x => x.FirstName).NotEmpty();
-            RuleFor(x => x.Gender).NotEmpty();
-            RuleFor(x => x.HouseNumber).NotEmpty();
-            RuleFor(x => x.LastName).NotEmpty();
-            RuleFor(x => x.Street).NotEmpty();
-            
-            RuleFor(x => x.ZipCode).NotEmpty()
-                .Matches("[0-9]{2}-[0-9]{3}").WithMessage("'{PropertyName}' must match pattern 00-000");
-
+            RuleFor(x => x.Gender).Custom((gender, context) =>
+            {
+                if (!Enum.IsDefined(gender.Value))
+                    context.AddFailure($"Gender with given Id = {gender.Value} is not defined");
+            });
         }
     }
 }
